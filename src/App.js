d04c5bf0bb0199598a6b940c0c7ea7022aac0eac
Laurynas <laupuskunigis@gmail.com>
2019-03-21 02:31:17 +0100

    Added Quote-box and a few child elements

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      author: "",
+      quote: ""
+    }
+  }
   render() {
     return (
       <div className="App">
-        
+        <div id="quote-box">
+          <p id="text"></p>
+          <p id="author"></p>
+          <button id="new-quote" >New quote</button>
+          <button id="tweet-quote" >Tweet</button>
+        </div>
       </div>
     );
   }
